0a5d02d4dc25bf061727b2a4fd4837f97c554a69
Iandmee <milaevpro1@gmail.com>
2023-03-31 14:35:59 +0200

    change the behavior while meeting an invalid signature of the CodeView category. Previous commit - exit the program, now - just skip this directory. Use the previous commit, for the instant exit if it suits you more.

--- PDB-extractor/PathExtractor.cs
+++ PDB-extractor/PathExtractor.cs
@@ -125,7 +125,9 @@ namespace PdbExtractor
                 signature = new String(copySubArray(rawDataPointerToCodeView, DWORD).Select(b => (char)b).ToArray());
                 if (signature != RSDS_SIGNATURE)
                 {
-                    throw new ArgumentException("Signature != RSDS in some CodeView directory");
+                    //throw new ArgumentException("Signature != RSDS in some CodeView directory");
+                    // in case, if you want just to skip "invalid" codeview directories you should keep it like this. In other cases uncomment the previous line, and comment next one.
+                    continue;
                 }
                 byte[] pathBytes = copySubArray(rawDataPointerToCodeView + PDB_PATH_OFFSET, sizeOfCodeViewData - PDB_PATH_OFFSET);
                 Array.Resize(ref pathBytes, Array.FindLastIndex(pathBytes, c => c != 0) + 1);
